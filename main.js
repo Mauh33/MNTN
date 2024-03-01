fe48b09e9d1881578200fd16188658b65bee4209
Morgane <morgane@freemiumplay.com>
2024-03-01 14:38:27 +0100

    progress bar on click animation done

--- main.js
+++ main.js
@@ -1,8 +1,28 @@
-document.querySelectorAll('a[href^="#"').forEach(anchor => {
+document.querySelectorAll(".progress-bloc a").forEach(anchor => {
   anchor.addEventListener("click", function (e) {
     e.preventDefault();
 
-    document.querySelector(this.classList.add("active"));
+    document.querySelectorAll(".progress-bloc a").forEach(element => {
+      element.classList.remove("active");
+    });
+
+    this.classList.add("active");
+
+    let index = Array.from(this.parentNode.children).indexOf(this);
+
+    let progressBloc = document.querySelector(".progress-bloc");
+    progressBloc.classList.remove("active", "one", "two", "three", "four");
+
+    if (index === 0) {
+      progressBloc.classList.add("active", "one");
+    } else if (index === 1) {
+      progressBloc.classList.add("active", "two");
+    } else if (index === 2) {
+      progressBloc.classList.add("active", "three");
+    } else if (index === 3) {
+      progressBloc.classList.add("active", "four");
+    }
+
     document.querySelector(this.getAttribute("href")).scrollIntoView({
       behavior: "smooth",
       block: "start",
