65b920a3b4d9cef1856e947bcc891cace80caac2
Morgane <morgane@freemiumplay.com>
2024-03-06 16:06:34 +0100

    refacto in functions and scroll animation/calculate in progress

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
+let progressBloc = document.querySelector(".progress-bloc");
+
+function calculateScrollPercentage() {
+  let scrollTop = window.scrollY;
+  let windowHeight = window.innerHeight;
+  let fullHeight = document.body.clientHeight;
+
+  const percentageScrolled = (scrollTop / (fullHeight - windowHeight)) * 100;
+
+  return percentageScrolled;
+}
+
+window.addEventListener("scroll", function () {
+  let scrollPercentage = calculateScrollPercentage();
+  console.log(
+    "pourcentage de scroll par rapport à la hauteur de la page",
+    scrollPercentage
+  );
+  return scrollPercentage;
+});
+
 document.querySelectorAll(".progress-bloc a").forEach(anchor => {
   anchor.addEventListener("click", function (e) {
     e.preventDefault();
@@ -10,16 +31,15 @@ document.querySelectorAll(".progress-bloc a").forEach(anchor => {
 
     let index = Array.from(this.parentNode.children).indexOf(this);
 
-    let progressBloc = document.querySelector(".progress-bloc");
     progressBloc.classList.remove("active", "one", "two", "three", "four");
 
-    if (index === 0) {
+    if (index === 0 || scrollPercentage <= 24) {
       progressBloc.classList.add("active", "one");
-    } else if (index === 1) {
+    } else if (index === 1 || scrollPercentage <= 49) {
       progressBloc.classList.add("active", "two");
-    } else if (index === 2) {
+    } else if (index === 2 || scrollPercentage <= 74) {
       progressBloc.classList.add("active", "three");
-    } else if (index === 3) {
+    } else if (index === 3 || scrollPercentage <= 99) {
       progressBloc.classList.add("active", "four");
     }
 
@@ -29,3 +49,5 @@ document.querySelectorAll(".progress-bloc a").forEach(anchor => {
     });
   });
 });
+
+function animateProgressbar() {}
